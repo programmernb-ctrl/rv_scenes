db201ccda8bc157ea6bda5767c7d0247851f77bd
thirst <141086417+programmernb-ctrl@users.noreply.github.com>
2025-06-04 04:50:31 +0200

    (fix): removed typo in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,7 @@ description 'Utility Script for handling synchronised network scenes.'
 repository 'https://github.com/programmernb-ctrl/rv_scenes'
 name 'rv_scenes'
 
-client_script 'cl_scenes.lua'
+client_script 'client/cl_scenes.lua'
 shared_script '@ox_lib/init.lua'
 server_script 'server/main.lua'
 
